21f8ef057a2c7848a137dd85fe655efc6faabd05
Kiana Alessandra Villaera <kvvillaera@up.edu.ph>
2017-11-22 11:06:09 +0800

    Update Currently_On_Leave.php

--- Attendance and Personnel/Currently_On_Leave.php
+++ Attendance and Personnel/Currently_On_Leave.php
@@ -33,7 +33,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Luke's Foundation HR Department</a>
+                <a class="navbar-brand" href="../../">Luke's Foundation HR Department</a>
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
