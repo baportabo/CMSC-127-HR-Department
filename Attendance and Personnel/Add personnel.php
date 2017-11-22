a61432fd64989aac2f0a1e0987f805f0b18655d0
Kiana Alessandra Villaera <kvvillaera@up.edu.ph>
2017-11-22 11:27:36 +0800

    Update Add personnel.php

--- Attendance and Personnel/Add personnel.php
+++ Attendance and Personnel/Add personnel.php
@@ -28,7 +28,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../../">Luke's Foundation HR Department</a>
+                <a class="navbar-brand" href="../../">Luke Foundation Inc., Database</a>
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
